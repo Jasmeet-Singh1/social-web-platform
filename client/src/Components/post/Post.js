8d8ef07b0f50e68337109933a34a58cac446297b
Jasmeet <jassoffline586@gmail.com>
2024-09-23 19:34:22 -0700

    Added Display and delete comment

--- client/src/Components/post/Post.js
+++ client/src/Components/post/Post.js
@@ -6,13 +6,14 @@ import { getPost } from '../../actions/post';
 import { Link, useParams } from 'react-router-dom';
 import PostItem from '../posts/PostItem';
 import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
 
 const Post = ({ getPost, post: { post, loading } }) => {
   const { id } = useParams();
 
   useEffect(() => {
     getPost(id);
-  }, [getPost]);
+  }, [getPost, id]);
 
   return loading || post === null ? (
     <Spinner />
@@ -23,6 +24,11 @@ const Post = ({ getPost, post: { post, loading } }) => {
       </Link>
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
+      <div className="comments">
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </div>
   );
 };
--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -161,7 +161,7 @@ export const addComment = (postId, formdata) => async (dispatch) => {
 
 export const deleteComment = (postId, commentId) => async (dispatch) => {
   try {
-    const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
 
     dispatch({
       type: REMOVE_COMMENT,
